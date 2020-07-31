8c7624593dc74aadf9e26e5a003f90eec01d3532
katepang13 <liuyhpangpang@gmail.com>
2020-07-31 17:32:35 +0800

    Update 0153-寻找旋转排序数组中的最小值.cpp

--- 0153.寻找旋转排序数组中的最小值/0153-寻找旋转排序数组中的最小值.cpp
+++ 0153.寻找旋转排序数组中的最小值/0153-寻找旋转排序数组中的最小值.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start+1<end) //判断条件也可以写成start<end,无所谓看个人习惯，只要在最后加以处理就行
-        {
-            int mid=(start+end)>>1;
-            if(nums[mid]<nums[end])
-            {
-                end=mid;
-            }
-            else if(nums[mid]>nums[end])
-            {
-                start=mid;
+        int n = nums.size();
+        if( n == 0 ) {
+            return -1;
+        }
+
+        int l = 0;
+        int r = n-1;
+        while( l < r ) {
+            int mid = l + (r-l)/2;
+            if( nums[mid] < nums[r] ) {
+                r = mid;
+            } else {
+                l = mid+1;
             }
         }
-        return min(nums[start],nums[end]);//最后答案定位在了两个数字，小的那个即为答案
+
+        return nums[l];
     }
 };
