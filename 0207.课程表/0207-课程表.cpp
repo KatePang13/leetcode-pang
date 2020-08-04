130af862cf6d73fea58964e26e26ae7c6e097278
katepang13 <liuyhpangpang@gmail.com>
2020-08-04 11:24:22 +0800

    判断有向无环图 -- DFS   [0207课程表]

--- 0207.课程表/0207-课程表.cpp
+++ 0207.课程表/0207-课程表.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> inDegrees(numCourses);
-        vector<vector<int>> adjacents(numCourses);
-        queue<int> zeroQueue;
-        int num = numCourses;
-
-        for( auto& edge : prerequisites ) {
-            inDegrees[edge[0]]++;
-            adjacents[edge[1]].push_back(edge[0]);
-        }
-
-        //所有原始入度为0的节点放入zeroQueue;
-        for( int i=0; i<numCourses; i++ ) {
-            if( inDegrees[i] == 0 ) {
-                zeroQueue.push(i);
-                num--;
-            }
-        }
-
-        while( !zeroQueue.empty() ) {
-            int tmp = zeroQueue.front();
-            zeroQueue.pop();
-            //删除当前节点，意味着邻接节点的 入度-1
-            //遍历邻接节点，将 入度==0 的节点加入 zeroQueue
-            for( int j=0; j<adjacents[tmp].size(); j++ ) {
-                inDegrees[adjacents[tmp][j]]--;
-                if(inDegrees[adjacents[tmp][j]] == 0 ) {
-                    zeroQueue.push(adjacents[tmp][j]);
-                    num--;
-                }
-            }
-        }
-
-        if( num==0 ) {
+        //prerequisites 为空，表示课程间不存在前后关系;
+        if( prerequisites.size() == 0) {
             return true;
         }
 
-        return false;
+        int m = prerequisites.size();
+        int n = prerequisites[0].size();
+        vector<int> flags(numCourses); //0-初识状态；1-在本次路径被访问过；-1-在其他路径被访问过;
+        vector<vector<int>> adjacents(numCourses);
+        for( auto& edge : prerequisites ) {
+            adjacents[edge[1]].push_back(edge[0]);
+        }
+
+        for( int i=0; i<numCourses; i++ ) {
+            if( dfs(adjacents, flags, i) == false ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool dfs( vector<vector<int>>& adjacents, vector<int>& flags, int i) {
+        //在别的深度路径中出现过
+        if( flags[i] == -1 ) { 
+            return true;
+        }
+        //在一次深度路径中再次出现，有环;
+        if( flags[i] == 1 ) { 
+            return false;
+        }
+        flags[i] = 1;
+
+        for( int j : adjacents[i] ) {
+            if( dfs(adjacents, flags, j) == false ) {
+                return false;
+            }
+        }
+        flags[i] = -1;
+        return true;
     }
 };
