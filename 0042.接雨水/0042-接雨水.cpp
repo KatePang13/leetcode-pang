48f796f884c045811908feccd445363fb560fb66
katepang13 <liuyhpangpang@gmail.com>
2020-08-05 11:15:32 +0800

    Update 0042-接雨水.cpp

--- 0042.接雨水/0042-接雨水.cpp
+++ 0042.接雨水/0042-接雨水.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int area = 0;
-        stack<int> s;
-        int i=0;
+        int n = height.size();
+        if( n< 2 ) {
+            return 0;
+        }
 
-        for( ; i<height.size(); i++ ) {
-            while( !s.empty() && height[i] >= height[s.top()] ) {
-                int prev = s.top();
-                s.pop();
-                if( s.empty() ) {
+        int area = 0;
+        stack<int> st;
+
+        st.push(0);
+        //[1, n] 因为 0 和 n+1 是填充的，不会对结果产生影响;
+        for( int i=1; i<n; i++ ) {
+            while( !st.empty() && height[i] >= height[st.top()] ) {
+                int cur = st.top();
+                st.pop();
+                if( st.empty() ) {
                     break;
                 }
 
-                int l = s.top();
+                int l = st.top();
                 int r = i;
-                int w = r-l-1;
-                int h = min( height[l], height[r] ) - height[prev] ;
-                area += w*h;
+                int h = min( height[l], height[r]) - height[cur];
+                int w = r-l-1; 
+                area += h*w;
             }
-            s.push(i);
+            st.push(i);
         }
 
         return area;
