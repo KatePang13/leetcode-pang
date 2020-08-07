97460aee53afceef038c823cf0a90a899091c82a
katepang13 <liuyhpangpang@gmail.com>
2020-08-07 11:44:43 +0800

    Update 0100-相同的树.cpp
    每日一题

--- 0100.相同的树/0100-相同的树.cpp
+++ 0100.相同的树/0100-相同的树.cpp
@@ -20,10 +20,6 @@ public:
             return false;
         }
 
-        if( p->val != q->val ) {
-            return false;
-        }
-
-        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
