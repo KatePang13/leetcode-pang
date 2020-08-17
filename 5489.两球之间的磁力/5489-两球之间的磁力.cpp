0dde6714d0eddb30d46bfe476bc8f3561c43867c
katepang13 <liuyhpangpang@gmail.com>
2020-08-17 11:58:06 +0800

    二分查找-进阶练习

--- 5489.两球之间的磁力/5489-两球之间的磁力.cpp
+++ 5489.两球之间的磁力/5489-两球之间的磁力.cpp
@@ -1,23 +1,39 @@
 class Solution {
-    int can(vector<int>&a, int c) {
-        int la = -1000000000;
-        int S = 0;
-        for (auto i : a)
-            if (i-la >= c) {
-                S ++;
-                la = i;
-            }
-        return S;
-    }
 public:
-    int maxDistance(vector<int>& a, int m) {
-        sort(a.begin(), a.end());
-        int le = 0, ri = 1000000000;
-        while (le <= ri) {
-            int mid = (le+ri)/2;
-            if (can(a, mid) >= m) le = mid+1;
-            else ri = mid-1;
+    int maxDistance(vector<int>& position, int m) {
+        int n = position.size();
+        sort( position.begin(), position.end() );
+        int right = (position[n-1] - position[0])/(m-1);
+        int left = 1;
+        int ans = 1;
+        cout << right << ' ' << left << endl;
+
+        while( left <= right ) {
+            int mid = left + (right-left)/2;
+            if( check( position, mid, m) ) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return ri;
+
+        return ans;
+    }
+
+    bool check(vector<int>& position, int distance, int m) {
+        int n = position.size();
+        int prev = 0;
+        int count = 1;
+        for( int i =1; i<n; i++ ) {
+            if( position[i] - position[prev] >= distance ) {
+                count++;
+                prev = i;
+            }
+            if( count == m ) {
+                return true;
+            }
+        }
+        return false;
     }
 };
