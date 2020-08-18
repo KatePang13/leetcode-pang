7bde761572c9dfa5f11b90fca09e5f8153cb6e4d
katepang13 <liuyhpangpang@gmail.com>
2020-08-18 11:14:43 +0800

    Update 0109-有序链表转换二叉搜索树.cpp
    每日一题

--- 0109.有序链表转换二叉搜索树/0109-有序链表转换二叉搜索树.cpp
+++ 0109.有序链表转换二叉搜索树/0109-有序链表转换二叉搜索树.cpp
@@ -3,7 +3,9 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 /**
@@ -12,45 +14,38 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        while( !head ) {
-            return NULL;
-        } 
+        return dfs(head, nullptr);
+    }
 
-        //[left][mid][right]寻找中点，划分左右子树;
-        //快慢指针，流程结束时,slow指向中间节点;
-        //[head, slow->prev, NULL]  [slow] [slow->next, NULL]
-        ListNode* slowPrev = NULL;
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        int leftCount = 0;
-        int rightCount = 0;
-        while( fast ) {
-            slowPrev =slow;
+    TreeNode* dfs(ListNode* begin, ListNode* end) {
+        if( begin == nullptr ) {
+            return nullptr;
+        }
+        if( begin == end ) {
+            return nullptr;
+        }
+
+        ListNode* slow = begin;
+        ListNode* fast = begin;
+        while( fast != end && fast->next !=end ) {
             slow = slow->next;
-            leftCount++;
-
-            fast = fast->next;
-            if( fast ) {
-                fast = fast->next;
-                rightCount++;
-            }
+            fast = fast->next->next;
         }
 
         TreeNode* root = new TreeNode(slow->val);
-        if( leftCount > 0 ) {
-            slowPrev->next = NULL;
-            root->left = sortedListToBST(head);
-        }
-        if( rightCount > 0 ) {
-            root->right = sortedListToBST( slow->next );
-        }
-
+        root->left = dfs(begin, slow);
+        root->right = dfs(slow->next, end);
         return root;
     }
+
 };
+
+
