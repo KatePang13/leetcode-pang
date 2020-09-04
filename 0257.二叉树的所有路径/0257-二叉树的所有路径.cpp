7d06baaa7a96c7c430eaad34df33f0b47db65094
katepang13 <liuyhpangpang@gmail.com>
2020-09-04 21:12:23 +0800

    2020-09-04

--- 0257.二叉树的所有路径/0257-二叉树的所有路径.cpp
+++ 0257.二叉树的所有路径/0257-二叉树的所有路径.cpp
@@ -9,21 +9,29 @@
  */
 class Solution {
 public:
+    vector<string> ans;
+public:    
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector <string> res;
-        if(root==NULL) return res;//递归出口，当root为空时，返回空
-        if(root->left==NULL&&root->right==NULL) {//当root为叶子节点时，将叶子结点的val放入res，再返回
-            res.push_back(to_string(root->val)); 
-            return res;
+        dfs(root, "", true);
+        return ans;
+    }
+
+    void dfs(TreeNode* root, string buf, bool isRoot) {
+        if( !root ) {
+            return;
         }
-        vector <string> lefts=binaryTreePaths(root->left);//递归过程
-        for(int i=0;i<lefts.size();i++) {//求左子树的路径
-            res.push_back(to_string(root->val)+"->"+lefts[i]);//将根节点的值放入res，按题目要求储存
+
+        if( !isRoot ) {
+            buf += "->";
         }
-        vector <string> rights=binaryTreePaths(root->right);//求右子树的路径
-        for(int i=0;i<rights.size();i++) {
-            res.push_back(to_string(root->val)+"->"+rights[i]);
+
+        buf += to_string(root->val);
+        if( !root->left && !root->right ) {
+            ans.push_back(buf);
+            return;
         }
-        return res;
+
+        dfs(root->left, buf, false);
+        dfs(root->right, buf, false);
     }
 };
