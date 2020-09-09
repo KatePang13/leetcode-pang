abe5c98056468420da8b239010f61e26a7cf6be7
katepang13 <liuyhpangpang@gmail.com>
2020-09-09 15:24:17 +0800

    Update 0050-Pow(x,n).cpp

--- 0050.Pow(x,n)/0050-Pow(x,n).cpp
+++ 0050.Pow(x,n)/0050-Pow(x,n).cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if( x == 0 ){
-            return 0;
+        if( n == 0 ) {
+            return 1;
         }
 
-        long long N = n;
-        if( N==0){
-            return 1;
-        } else if ( n<0 ){
+        if( x == 0 || x == 1 ) {
+            return x;
+        }
+
+        long N = n;
+        if( n<0 ) {
             x = 1/x;
             N = -N;
         }
-        
-        return myPowCore( x, N );
+
+        return myPowCore(x, N);
     }
 
-    double myPowCore(double x, long long n) {
-        double tmp = x;
+    double myPowCore(double x, long n) {
+        double sub = x;
         double ans = 1.0;
-        while( n > 0 ){
-            if( n%2 ){
-                ans *= tmp;
-            }
 
-            tmp *= tmp;
-            n /= 2;
+        while( n>0 ) {
+            if( n%2 == 1 ) {
+                ans *= sub;
+            }
+                
+            sub *= sub;
+            n /= 2 ;
         }
 
         return ans;
