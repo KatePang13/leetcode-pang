605deb34b17cdfd44026c1ad978e335a6d740294
katepang13 <liuyhpangpang@gmail.com>
2020-09-13 15:20:59 +0800

    2020-09-13

--- 0079.单词搜索/0079-单词搜索.cpp
+++ 0079.单词搜索/0079-单词搜索.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    int dir[4][4]={{-1,0},{1,0},{0,-1},{0,1}};
-    
+    int dirs[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0,-1} };
+public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n));
-        
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(dfs(i,j,0,board,word,visited))
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> vis( m, vector<bool>(n) ); 
+
+        for( int x=0; x<m; x++ ) {
+            for( int y=0; y<n; y++ ) {
+                if( dfs( board, vis, x, y, word, 0) ) {
                     return true;
+                }
             }
         }
+
         return false;
     }
-    
-    bool dfs(int x,int y,int index,vector<vector<char>>& board,string &word,vector<vector<bool>>& visited){
-        if(index==word.size()-1){
-            return word[index]==board[x][y];
+
+    bool dfs(vector<vector<char>>& board, vector<vector<bool>>& vis, int x, int y, string& word, int index ) {
+        if( board[x][y] != word[index] ) {
+            return false;
         }
-        
-        if(word[index]==board[x][y]){
-            visited[x][y]=true;
-            for(int i=0;i<4;i++){
-                int new_x=x+dir[i][0];
-                int new_y=y+dir[i][1];
-                
-                if(new_x>=0&&new_x<board.size()&&new_y>=0&&new_y<board[0].size()&&!visited[new_x][new_y])
-                    if(dfs(new_x,new_y,index+1,board,word,visited))
-                        return true;
+
+        if( index == word.size() -1 ) {
+            return true;
+        }
+
+        vis[x][y] = true;
+        int m = board.size();
+        int n = board[0].size();
+        for( int i=0; i<4; i++ ) {
+            int nx = x + dirs[i][0];
+            int ny = y + dirs[i][1];
+            if( nx >=0 && nx < m && ny>=0 && ny < n && !vis[nx][ny] ) {
+                if(dfs( board, vis, nx, ny, word, index+1 )) {
+                    return true;
+                }
             }
-            visited[x][y]=false;
-            
-        }
+        } 
+        vis[x][y] = false;
         return false;
     }
 };
