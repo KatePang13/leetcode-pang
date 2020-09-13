2509d15f61337d4b3e0fbc9bc43f517b2be9561f
katepang13 <liuyhpangpang@gmail.com>
2020-09-13 15:20:53 +0800

    2020-09-12

--- 0637.二叉树的层平均值/0637-二叉树的层平均值.cpp
+++ 0637.二叉树的层平均值/0637-二叉树的层平均值.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {   
+class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         vector<double> ans;
@@ -17,20 +17,21 @@ public:
 
         queue< pair<TreeNode*, int> > q;
         q.push( {root, 0} );
-        vector<int> nums;
-        int lastLevel = 0;
+        double levelSum = 0;
+        int levelCnt = 0;
 
         while( !q.empty() ) {
             TreeNode* node = q.front().first;
             int level = q.front().second;
             q.pop();
+            levelSum += node->val;
+            levelCnt++;
 
-            if( lastLevel != level ) {
-                compute(nums, ans);
-                nums.clear();
-                lastLevel = level;
+            if( q.empty() || q.front().second > level ) {
+                ans.push_back(levelSum/levelCnt );
+                levelSum = 0.0;
+                levelCnt = 0;
             }
-            nums.push_back(node->val);
 
             if( node->left ) {
                 q.push({node->left, level+1});
@@ -38,24 +39,8 @@ public:
             if( node->right ) {
                 q.push({node->right, level+1});
             }
-        }
+        } 
 
-        compute(nums, ans);
         return ans;
     }
-
-    void compute(vector<int>& nums, vector<double>& ans) {
-            int n = nums.size();
-            if( n == 0 ) {
-                return ;
-            }
-
-            long long sum = 0;
-            for( auto num : nums ) {
-                sum += num;
-            }
-            double avg = double(sum) / n;
-            ans.push_back(avg);
-    } 
-
 };
