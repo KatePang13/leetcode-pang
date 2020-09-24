de180467cbc97dc8a8e7166a9a0fb278bf1ff9e2
katepang13 <liuyhpangpang@gmail.com>
2020-09-24 10:00:04 +0800

    0617

--- 0617.合并二叉树/0617-合并二叉树.cpp
+++ 0617.合并二叉树/0617-合并二叉树.cpp
@@ -10,19 +10,21 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if( !t1 && !t2) {
+        if( !t1 && !t2 ) {
             return NULL;
         }
 
-        if( !t1 ) {
-            return t2;
-        } else if( !t2 ) {
-            return t1;
+        int val = 0;
+        if( t1 ) {
+            val += t1->val;
+        } 
+        if( t2 ) {
+            val += t2->val;
         }
 
-        t1->val += t2->val;
-        t1->left = mergeTrees(t1->left, t2->left);
-        t1->right = mergeTrees(t1->right, t2->right);
-        return t1;
+        TreeNode* root = new TreeNode(val);
+        root->left = mergeTrees( (t1 ? t1->left : NULL), (t2 ? t2->left : NULL) );
+        root->right = mergeTrees( (t1 ? t1->right : NULL), (t2 ? t2->right : NULL) );
+        return root;
     }
 };
