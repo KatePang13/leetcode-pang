b815ecfcd1bbe8024dccb20b85374f92e8fa5458
katepang13 <liuyhpangpang@gmail.com>
2020-10-13 09:41:36 +0800

    2020-10-13

--- 0024.两两交换链表中的节点/0024-两两交换链表中的节点.cpp
+++ 0024.两两交换链表中的节点/0024-两两交换链表中的节点.cpp
@@ -3,37 +3,34 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode tmphead(0);
-        tmphead.next = head;
-        ListNode* prev = &tmphead;
-        ListNode* slow = head;
-        ListNode* fast = NULL;
-        if( slow ) {
-            fast = slow->next;
+        if( !head || !head->next ) {
+            return head;
         }
 
+        ListNode* slow = head ;
+        ListNode* fast = head->next ;
+        ListNode tmphead(0);
+        ListNode* prev = &tmphead;
+
         while( fast ) {
-            ListNode* next = fast->next;
             prev->next = fast;
+            slow->next = fast->next;
             fast->next = slow;
-            slow->next = next;
-
-            if( !slow->next ) {
-                break;
-            }
-            if(!slow->next->next) {
-                break;
-            }
-
             prev = slow;
-            slow = slow->next;
-            fast = slow->next;
+
+            slow = prev->next;
+            fast = slow;
+            if( fast ) {
+                fast = fast->next;
+            }
         }
 
         return tmphead.next;
