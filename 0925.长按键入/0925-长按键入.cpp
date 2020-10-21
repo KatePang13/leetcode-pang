073d83bb6cbb6cc7e948bed427778d3239da0418
katepang13 <liuyhpangpang@gmail.com>
2020-10-21 09:44:19 +0800

    Update 0925-长按键入.cpp

--- 0925.长按键入/0925-长按键入.cpp
+++ 0925.长按键入/0925-长按键入.cpp
@@ -4,25 +4,13 @@ public:
         int nn = name.length();
         int nt = typed.length();
 
-        if( nn > nt ) {
-            return false;
-        }
-
-        if( nn == 0 && nt == 0 ) {
-            return true;
-        }
-
-        if( name[0] != typed[0] ) {
-            return false;
-        }
-
-        int i=1;
-        int j=1;
+        int i=0;
+        int j=0;
         while( j< nt ) {
             if( i<nn && name[i] == typed[j] ) {
                 i++;
                 j++;
-            } else if( name[i-1] == typed[j] ) {
+            } else if( i>0 && name[i-1] == typed[j] ) {
                 j++;
             } else {
                 return false;
