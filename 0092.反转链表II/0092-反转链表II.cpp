b9f51d83ff1dda96f8e3d44d3d160d77faf5f56e
刘炎宏 <liuyanhong@bonree.com>
2021-03-29 21:58:57 +0800

    2021-03-29

--- 0092.反转链表II/0092-反转链表II.cpp
+++ 0092.反转链表II/0092-反转链表II.cpp
@@ -8,40 +8,30 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* current, ListNode* next,ListNode** end, int m, int n ,int step)
-    {
-        if( step == n)
-        {
-            *end = next;
-            return current;
-        }
-
-        ListNode* newstart = reverse(next,next->next,end, m,n, step+1);
-        next->next = current;
-        
-        return newstart;
-    }
-
     ListNode* reverseBetween(ListNode* head, int m, int n) {
+        
         ListNode tmphead(0);
         tmphead.next = head;
-        ListNode *current = &tmphead;
-        ListNode *pre = NULL;
-        ListNode* end = NULL;
+        ListNode* cur = &tmphead;
+        ListNode* mPre = NULL;
+        ListNode* pre = NULL;
+        ListNode* next = NULL;
 
-        int i = 0;
-
-        for(;i<m; i++)
-        {
-            pre = current;
-            current = current->next;
+        for( int i=0 ;i<n+1; i++ ){
+            if( i<m ){
+                mPre = pre = cur;
+                cur = cur->next;
+            }else{
+                next = cur->next;
+                cur->next =pre;
+                pre =cur;
+                cur = next;
+            }
         }
 
-        ListNode* newstart = reverse(current,current->next, &end, m , n, i);
-        pre->next->next = end;
-        pre->next= newstart;
+        mPre->next->next = cur;
+        mPre->next = pre;
+
         return tmphead.next;
     }
-
-    
 };
