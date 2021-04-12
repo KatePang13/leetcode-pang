a5446ce0906a6c7b209e4fd6f7098688838ec419
刘炎宏 <liuyanhong@bonree.com>
2021-04-12 15:17:04 +0800

    2021-04-12

---
+++ 0179.最大数/0179-最大数.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        string ans;
+        sort(nums.begin(), nums.end(), [](const int& x, const int& y) {
+            long sx = 10;
+            long sy = 10;
+            while( sx <= x ) {
+                sx *= 10;
+            }
+            while( sy <= y ) {
+                sy *= 10;
+            }
+            
+            return x*sy + y > y*sx + x;
+        } );
+
+        if( nums[0] == 0 ) {
+            return "0";
+        }
+
+        for( int num : nums ) {
+            ans += to_string(num);
+        }
+
+        return ans;
+    }
+};
