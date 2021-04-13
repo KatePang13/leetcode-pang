ab2f2aeb08b1ca3c52fa46a4e59a111660f4e7d7
刘炎宏 <liuyanhong@bonree.com>
2021-04-13 16:47:31 +0800

    2021-04-13

--- 0783.二叉搜索树节点最小距离/0783-二叉搜索树节点最小距离.cpp
+++ 0783.二叉搜索树节点最小距离/0783-二叉搜索树节点最小距离.cpp
@@ -1,33 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
-public:
-    int minDiffInBST(TreeNode* root) {
-        vector<int> valVec;
-        dfs(root, valVec);
-        int minDiff = INT_MAX;
-        for(int i=1; i<valVec.size(); i++){
-            int diff = valVec[i]- valVec[i-1];
-            minDiff = min(minDiff, diff); 
-        }
-
-        return minDiff;
-    }
-
-    void dfs(TreeNode* root, vector<int>& valVec) {
-        if( !root ) {
-            return;
-        }
-
-        dfs(root->left, valVec);
-        valVec.push_back(root->val);
-        dfs(root->right, valVec);
-    }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDiff = INT_MAX;
+    int prev = -1;
+
+    int minDiffInBST(TreeNode* root) {
+        dfs(root);
+        return minDiff;
+    }
+
+    void dfs(TreeNode* root) {
+        if( !root ) {
+            return;
+        }
+
+        dfs(root->left);
+
+        if( prev >= 0 ) {
+            int diff = root->val - prev;
+            minDiff = min( diff, minDiff);
+        } 
+        prev = root->val;
+
+        dfs(root->right);
+    }
 };
