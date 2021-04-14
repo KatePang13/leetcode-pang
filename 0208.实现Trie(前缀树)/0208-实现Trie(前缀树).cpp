895e4a8add5f875a00981caed403394e15b32d38
刘炎宏 <liuyanhong@bonree.com>
2021-04-14 16:17:59 +0800

    2021-04-14

--- 0208.实现Trie(前缀树)/0208-实现Trie(前缀树).cpp
+++ 0208.实现Trie(前缀树)/0208-实现Trie(前缀树).cpp
@@ -1,62 +1,63 @@
-struct Node {
-    vector<Node*> childrens;
-    bool isEnd;
-    Node() : childrens(26, NULL), isEnd(false) {};
-};
-
-class Trie {
-public:
-    Node head;
-
-public:
-    /** Initialize your data structure here. */
-    Trie() {
-    }
-    
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        Node* pNode = &head;
-        for( auto& ch : word ) {
-            int iCh = ch - 'a';
-            if( !pNode->childrens[iCh] ) {
-                pNode->childrens[iCh] = new Node();
-            }
-            pNode = pNode->childrens[iCh];
-        }
-        pNode->isEnd = true;
-    }
-    
-    /** Returns if the word is in the trie. */
-    bool search(string word) {
-        Node* pNode = &head;
-        for( auto& ch : word ) {
-            int iCh = ch - 'a';
-            if( !pNode->childrens[iCh] ) {
-                return false;
-            }
-            pNode = pNode->childrens[iCh];
-        }
-        return pNode->isEnd;
-    }
-    
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
-        Node* pNode = &head;
-        for( auto& ch : prefix ) {
-            int iCh = ch - 'a';
-            if( !pNode->childrens[iCh] ) {
-                return false;
-            }
-            pNode = pNode->childrens[iCh];
-        }
-        return true;
-    }
-};
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
+
+struct Node {
+    vector<Node*> child;
+    bool isWord;
+    Node(): child(26, NULL), isWord(false) {};
+};
+
+class Trie {
+
+Node root;
+
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node* pNode = &root;
+        for( char ch : word ) {
+            int ich = ch - 'a';
+            if( pNode->child[ich] == 0 ) {
+                pNode->child[ich] = new Node();
+            }
+            pNode = pNode->child[ich];
+        }
+        pNode->isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node* pNode = &root;
+        for( char ch : word ) {
+            int ich = ch - 'a';
+            if( pNode->child[ich] == 0 ) {
+                return false;
+            }
+            pNode = pNode->child[ich];
+        }
+        return pNode->isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node* pNode = &root;
+        for( char ch : prefix ) {
+            int ich = ch - 'a';
+            if( pNode->child[ich] == 0 ) {
+                return false;
+            }
+            pNode = pNode->child[ich];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
  */
