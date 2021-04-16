f4519c7ec5054da9f13b461065f8c12fa417a123
刘炎宏 <liuyanhong@bonree.com>
2021-04-16 10:17:28 +0800

    2021-04-16

--- 0198.打家劫舍/0198-打家劫舍.cpp
+++ 0198.打家劫舍/0198-打家劫舍.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if( n == 0 ) {
-            return 0;
-        }
-        if( n ==1 ) {
-            return nums[0];
-        }
-
-        vector<int> f(n+1);
-        f[0] = 0;
-        f[1] = nums[0];
-
-        for( int i=1; i<n; i++) {
-            f[i+1] = max( f[i], f[i-1] + nums[i]);
-        }
-
-        return f[n];
-    }
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if( n == 0 ) {
+            return 0;
+        }
+        if( n == 1 ) {
+            return nums[0];
+        }
+
+        vector<int> gain(n, 0);
+
+        gain[0] = nums[0];
+        gain[1] = max(nums[0], nums[1]);
+        for( int i=2; i<n; i++ ) {
+            gain[i] = max(gain[i-1], gain[i-2]+nums[i]);
+        }
+
+        return gain[n-1];
+    }
 };
