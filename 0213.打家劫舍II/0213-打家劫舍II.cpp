a998c3a263a5a96b80286fcb6830a59f0ad36f42
刘炎宏 <liuyanhong@bonree.com>
2021-04-16 10:42:55 +0800

    2021-04-16

--- 0213.打家劫舍II/0213-打家劫舍II.cpp
+++ 0213.打家劫舍II/0213-打家劫舍II.cpp
@@ -1,24 +1,26 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if( n == 0 ) {
-            return 0;
-        }
-        if( n == 1 ) {
-            return nums[0];
-        }
-        vector<vector<int>> f(n, vector<int>(2) );  //f[i][0] 包含 nums[0]的情况， f[i][1] 不包含 nums[0]的情况;
-        f[0][0] = nums[0];
-        f[0][1] = 0;
-        f[1][0] = max(nums[0], nums[1]);
-        f[1][1] = nums[1];
-
-        for( int i=2; i<n; i++) {
-            f[i][0] = max( f[i-1][0], f[i-2][0] + nums[i] );
-            f[i][1] = max( f[i-1][1], f[i-2][1] + nums[i] );
-        }
-        
-        return max( f[n-2][0], f[n-1][1]);
-    }
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if( n == 0 ) {
+            return 0;
+        }
+        if( n == 1 ) {
+            return nums[0];
+        }
+
+        vector<vector<int>> gain(2, vector<int>(n) );
+        gain[0][0] = nums[0];
+        gain[0][1] = max(nums[0], nums[1]);
+        gain[1][0] = 0;
+        gain[1][1] = nums[1];
+
+        for( int i=2; i<n; i++) {
+            gain[0][i] = max(gain[0][i-1], gain[0][i-2] + nums[i] );
+            gain[1][i] = max(gain[1][i-1], gain[1][i-2] + nums[i] );
+        }
+
+
+        return max( gain[0][n-2], gain[1][n-1] );
+    }
 };
