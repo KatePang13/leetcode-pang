1f1f68093add60ce2c0eb7acab1ac33eb48ba565
刘炎宏 <liuyanhong@bonree.com>
2021-04-16 11:33:06 +0800

    2021-04-16

--- 0337.打家劫舍III/0337-打家劫舍III.cpp
+++ 0337.打家劫舍III/0337-打家劫舍III.cpp
@@ -1,30 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
-public:
-    int rob(TreeNode* root) {    
-        auto result = dfs(root);
-        return result.first;
-    }
-
-    pair<int, int> dfs( TreeNode* root) {
-        if( !root ) {
-            return {0, 0};
-        }
-
-        auto leftResult = dfs(root->left);
-        auto rightResult = dfs(root->right);
-
-        int curWithSelf = root->val + leftResult.second + rightResult.second;
-        int curWithoutSelf = leftResult.first + rightResult.first;
-        int curMax = max( curWithSelf, curWithoutSelf );
-        return { curMax, curWithoutSelf};
-    }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        auto [ rootInclude, rootExclude ] = dfs(root);
+        return max( rootInclude,  rootExclude);
+    }
+
+    pair<int, int> dfs(TreeNode* root) {
+        if( !root) {
+            return {0, 0};
+        }
+
+        auto [l, lExclude] = dfs(root->left);
+        auto [r, rExclude] = dfs(root->right);
+
+        return {root->val+lExclude+rExclude, max(l, lExclude) + max(r, rExclude) };
+    }
 };
